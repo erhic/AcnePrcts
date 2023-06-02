65aa3758d00719b9bf0dad21192ad97037323203
erhic <ericngugi24@gmail.com>
2023-06-02 18:58:12 +0300

    added an array to loop through and display in the dom if the array has elements or isnt empty

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,35 @@
+import  {Component}from '@angular/core'
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+
+
+})
+
+export class ProductListComponent{
+  pageTitle:string = 'Product List';
+ products:any[] =[
+
+  {
+    'productId':2,
+    'productNmae': 'Garden Cart',
+    'productCode': 'GDN-0023',
+    'releaseDate': 'March 18, 2021',
+    'description':'15 gallons capacity rolling',
+    'price': 23.43,
+    'starRating':4.2,
+    'imageUrl':'assets/images/garden_cart.png'
+  },
+  {
+    'productId':5,
+    'productNmae': 'Hammer',
+    'productCode': 'GDN-004389',
+    'releaseDate': 'June 18, 2023',
+    'description':'Steel Curved hammer',
+    'price': 6.43,
+    'starRating':4.8,
+    'imageUrl':'assets/images/hammer.png'
+  }
+ ];
+}
